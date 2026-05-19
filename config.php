b1dc635b99d6e03fb07a559b036259aaa52e36bb
lord5priso <tiomelajoel@gmail.com>
2026-05-19 23:29:11 +0100

    Update database connection settings in config.php

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-// config.php
-$servername = "sql103.infinityfree.com"; // Vérifie bien le numéro sur ton panel
-$username = "if0_41869322"; 
-$password = "TON_MOT_DE_PASSE"; // Mets ton vrai mot de passe ici
-$dbname = "if0_41869322_waste";  // Le nom de ta NOUVELLE base de données
+// config.php lié à Clever Cloud
+$host = "bxpxzcqwwefyyc6x3prx-mysql.services.clever-cloud.com"; 
+$user = "uk2xfalelpagoqxc";                 
+$pass = "T7APCCJydv9R3eYVY4LI";             
+$dbname = "bxpxzcqwwefyyc6x3prx";         
+$port = 3306; 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Changement du jeu de caractères en utf8mb4 pour éviter les problèmes d'accents (ex: Yaoundé)
-$conn->set_charset("utf8mb4");
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
-    die("La connexion a échoué : " . $conn->connect_error);
+    die("Échec de la connexion : " . $conn->connect_error);
 }
+
+// Encodage universel pour gérer proprement les accents des quartiers au Cameroun
+$conn->set_charset("utf8mb4");
 ?>
